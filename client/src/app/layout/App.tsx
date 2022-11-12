981f228feb3856e04008fe21e8c45acac14b2479
Julien <flynstone@live.ca>
2022-11-12 13:58:06 -0500

    added routing to client

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,4 +1,4 @@
-import { Container, createTheme, CssBaseline, ThemeProvider, Typography } from '@mui/material';
+import { Container, createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 import { useState } from 'react';
 import Catalog from '../../features/catalog/Catalog';
 import Header from './Header';
@@ -6,7 +6,11 @@ import '@fontsource/grandstander/300.css';
 import '@fontsource/grandstander/400.css';
 import '@fontsource/grandstander/500.css';
 import '@fontsource/grandstander/700.css';
-
+import { Route } from 'react-router-dom';
+import HomePage from '../../features/home/HomePage';
+import AboutPage from '../../features/about/AboutPage';
+import ContactPage from '../../features/contact/ContactPage';
+import ProductDetails from '../../features/catalog/ProductDetails';
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -34,7 +38,11 @@ function App() {
     <CssBaseline />
     <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
     <Container>
-      <Catalog />
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/catalog' component={Catalog} />
+      <Route path='/catalog/:id' component={ProductDetails} />
+      <Route path='/about' component={AboutPage} />
+      <Route path='/contact' component={ContactPage} />
     </Container>
   </ThemeProvider>
   );
